8a97e8afc743308ebaf619e3f772f1a14a901b97
omarwaheed2 <owaheed7@gmail.com>
2019-07-07 01:13:47 -0700

    Added BMW class that is derived from BaseVehicle and has some extra methods for BMWs. Also updated some namespaces and directories.

--- OOP/BaseVehicle/IBaseVehicle.cs
+++ OOP/BaseVehicle/IBaseVehicle.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OOP.Vehicle
+namespace OOP.BaseVehicle
 {
     interface IBaseVehicle
     {
--- OOP/Program.cs
+++ OOP/Program.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using OOP.Vehicle;
+using OOP.BaseVehicle;
+using OOP.BMW;
 
 namespace OOP
 {
@@ -13,16 +14,41 @@ namespace OOP
         {
             Console.WriteLine("Running OOP");
 
-            Console.WriteLine("Car # 1");
-            BaseVehicle baseVehicle = new BaseVehicle(4, "black", "BaseCar");
+            Console.WriteLine("Car # 1 of baseVehicle class");
+            BaseVehicle.BaseVehicle baseVehicle = new BaseVehicle.BaseVehicle(4, "black");
             baseVehicle.IgnitionOn();
             baseVehicle.Drive();
             baseVehicle.TurnLeft();
             baseVehicle.TurnRight();
             baseVehicle.Brake();
             baseVehicle.Stop();
-
             Console.ReadLine();
+
+            Console.WriteLine("Car # 2 of BMW class 5 series");
+            BMW.BMW bmw5Series = new BMW.BMW(4, "Navy", 5, "Manual");
+            bmw5Series.transmission();
+            bmw5Series.series();
+            bmw5Series.IgnitionOn();
+            bmw5Series.Drive();
+            bmw5Series.TurnLeft();
+            bmw5Series.TurnRight();
+            bmw5Series.Brake();
+            bmw5Series.Stop();
+            Console.ReadLine();
+
+            Console.WriteLine("Car # 3 of BMW class 7 series");
+            BMW.BMW bmw7Series = new BMW.BMW(4, "Red", 7, "Autmoatic");
+            bmw7Series.transmission();
+            bmw7Series.series();
+            bmw7Series.IgnitionOn();
+            bmw7Series.Drive();
+            bmw7Series.TurnLeft();
+            bmw7Series.TurnRight();
+            bmw7Series.Brake();
+            bmw7Series.Stop();
+            Console.ReadLine();
+
+
         }
     }
 }
