13c02fefc9c3682083e414e297523ba1d3fe00f1
omarwaheed2 <owaheed7@gmail.com>
2019-07-07 00:27:22 -0700

    Added interface and base class for BaseVehicle

---
+++ OOP/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OOP.Vehicle;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Running OOP");
+
+            Console.WriteLine("Car # 1");
+            BaseVehicle baseVehicle = new BaseVehicle(4, "black", "BaseCar");
+            baseVehicle.IgnitionOn();
+            baseVehicle.Drive();
+            baseVehicle.TurnLeft();
+            baseVehicle.TurnRight();
+            baseVehicle.Brake();
+            baseVehicle.Stop();
+
+            Console.ReadLine();
+        }
+    }
+}
